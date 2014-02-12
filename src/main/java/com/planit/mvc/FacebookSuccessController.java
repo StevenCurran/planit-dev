e4054f1d62f3e73b7b61bf9643e957ccaef758a0
StevenCurran <scurran14@qub.ac.uk>
2014-02-12 23:59:00 +0000

    update project to 1.7

---
+++ src/main/java/com/planit/mvc/FacebookSuccessController.java
@@ -0,0 +1,53 @@
+package com.planit.mvc;
+
+/**
+ * Created by Steven Curran on 12/02/14.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.brickred.socialauth.AuthProvider;
+import org.brickred.socialauth.Contact;
+import org.brickred.socialauth.SocialAuthManager;
+import org.brickred.socialauth.spring.bean.SocialAuthTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class FacebookSuccessController {
+
+    @Autowired
+    private SocialAuthTemplate socialAuthTemplate;
+
+    @RequestMapping(value = "/authSuccess")
+    public List<Contact> getRedirectURL(final HttpServletRequest request)
+            throws Exception {
+
+        List<Contact> contactsList = new ArrayList<>();
+        SocialAuthManager manager = socialAuthTemplate.getSocialAuthManager();
+        AuthProvider provider = manager.getCurrentAuthProvider();
+
+        contactsList = provider.getContactList();
+        if (contactsList != null && contactsList.size() > 0) {
+            for (Contact p : contactsList) {
+                if (!StringUtils.hasLength(p.getFirstName())
+                        && !StringUtils.hasLength(p.getLastName())) {
+                    p.setFirstName(p.getDisplayName());
+                }
+            }
+        }
+
+        
+
+        return contactsList;
+    }
+}
