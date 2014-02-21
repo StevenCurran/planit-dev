f241587839f4b6ec0303c60b1558a12e53ca2e99
StevenCurran <scurran14@qub.ac.uk>
2014-02-21 10:58:25 +0000

    check in some changes

--- src/main/java/com/planit/persistence/registration/UserRepository.java
+++ src/main/java/com/planit/persistence/registration/UserRepository.java
@@ -15,5 +15,5 @@ import java.util.List;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    User findByUsername(String username);
+    //User findByUsername(String username);
 }
