f988f1cd75e23bcea3f84a3811f173c3ec28dbf3
StevenCurran <scurran14@qub.ac.uk>
2014-03-02 20:55:18 +0000

    better query

--- src/main/java/com/planit/persistence/registration/UserRepository.java
+++ src/main/java/com/planit/persistence/registration/UserRepository.java
@@ -18,8 +18,15 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     //@Query("select User from event_user ")
    // public List<User> findEvents();
-//select distinct * from "Event"  e inner join "event_user" eu on eu.eventid = e.eventid inner join "User" u on u.userid = eu.userid where u.userid = '114700121025537154047'
+/*
 
-    //select distinct * from "User" u inner join event_user eu on eu.userid = u.userid inner join "Event" e on e.eventid = eu.eventid where e.eventid = '04u9nd81ua1c6eogdbnarjk0cs_20140228T100000Z'
+select distinct e.* from "Event" e inner join "event_user" eu on eu.eventid = e.eventid inner join "User" u on u.userid = eu.userid where u.userid = '114700121025537154047'
+
+select distinct u.* from "User" u inner join event_user eu on eu.userid = u.userid inner join "Event" e on e.eventid = eu.eventid where e.eventid = '04u9nd81ua1c6eogdbnarjk0cs_20140228T100000Z'
+
+
+
+ */
+
 
 }
