001372670599a4be776e4d0177b2e366d7e8966c
StevenCurran <scurran14@qub.ac.uk>
2014-03-26 14:55:48 +0000

    change the query

--- src/main/java/com/planit/persistence/registration/UserRepository.java
+++ src/main/java/com/planit/persistence/registration/UserRepository.java
@@ -18,7 +18,8 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     //    @Query(value = "select distinct e.* from \"Event\" e inner join \"event_user\" eu on eu.eventid = e.eventid inner join \"User\" u on u.userid = eu.userid where u.userid = ?1", nativeQuery = true)
 //from Event as e inner join e.eventid as eid where userid = :userId
-    @Query(value = "select distinct e from PlanitEvent e inner join e.attendees att where att.userId = ?1")
+
+    @Query(value = "select distinct e from PlanitEvent e inner join e.userEvents att where att.userId = ?1")
     public ArrayList<PlanitEvent> findEventsForUser(String userId);
 
 /*
